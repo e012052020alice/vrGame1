d1ba90954bccbe74f3318d7fb744b27f046112d9
e012052020alice <119144400+e012052020alice@users.noreply.github.com>
2023-04-22 13:36:28 +0800

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public ParticleSystem FireEffect;
+    private RaycastHit RH;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void FireEvent()
+    {
+        if (Physics.Raycast(transform.position,transform.forward,out RH,100f))
+        {
+            FireEffect.transform.position = RH.point;
+            FireEffect.transform.rotation = Quaternion.Euler(270, 0, 0);
+            FireEffect.Stop();
+            FireEffect.Play();
+        }
+    }
+}
